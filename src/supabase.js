d877b22beb77871e6286f72266cbf1d7ad926275
Kehila <77875506s@gmail.com>
2025-05-05 15:09:58 -0400

    Tienda3

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,9 @@
+// supabase.js
 import { createClient } from '@supabase/supabase-js';
-    import.meta.env.VITE_SUPABASE_URL,
-    import.meta.env.VITE_SUPABASE_ANON_KEY
 
+// Correcta extracción de variables de entorno
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+// Crear el cliente Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);

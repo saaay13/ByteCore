cf457184b9ecb941b8e2a3bde1c39f9704fe9a88
Kehila <77875506s@gmail.com>
2025-05-05 13:29:59 -0400

    FINAL

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://rpcfccukbgvjfthryawb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwY2ZjY3VrYmd2amZ0aHJ5YXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NDE2ODUsImV4cCI6MjA2MjAxNzY4NX0.vfazTiym5mqQFnYR2Vh5yj8-xtK0cR2AYyvJKQuNHnE'
+
+// Obtener las variables de entorno
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+// Crear una instancia de Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
